9344dc45b366a6a5826f077cc9040eaf44371108
Yuri Astrakhan <yurik@wikimedia.org>
2014-09-21 23:04:01 -0400

    Simplified error handling for dynamic graph re-rendering
    Change-Id: I32ec8347e8c4f04a589d5215d624bec24f21563d

--- js/graph.editor.js
+++ js/graph.editor.js
@@ -7,22 +7,25 @@
 				textbox = $('#wpTextbox1'),
 				context = textbox && textbox.data('wikiEditor-context');
 
-			if (!context || !context.evt || !context.evt.codeEditorSync || !context.fn) {
+			if (!context || !context.fn || !context.fn.getContents) {
 				return;
 			}
-			context.evt.codeEditorSync();
 			var content = context.fn.getContents();
 			if (oldContent === content) {
 				return;
 			}
 			oldContent = content;
-			var annotations = context.codeEditor.getSession().getAnnotations();
-			if (annotations.every(function(v) { return v.type !== 'error'; })) {
-				if (this.hasAttribute('spec'))
-					this.removeAttribute('spec');
-				var spec = $.parseJSON(content);
-				vg.parse.spec(spec, function(chart) { chart({el:el}).update(); });
+			var spec = null;
+			try {
+				spec = $.parseJSON(content);
+			} catch(err) {
 			}
+			if (spec === null) {
+				return;
+			}
+			if (this.hasAttribute('spec'))
+				this.removeAttribute('spec');
+			vg.parse.spec(spec, function(chart) { chart({el:el}).update(); });
 		});
 		// FIXME: This should be done on data modification, not on timer
 		setTimeout(arguments.callee, 300);
