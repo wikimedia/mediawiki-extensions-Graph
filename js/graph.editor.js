d126c414a167c5231e0766bedd2030431a690cfd
Yuri Astrakhan <yurik@wikimedia.org>
2014-09-21 23:17:54 -0400

    Even simpler error handling for auto-refresh
    Change-Id: I7a646eb16926b4aad249854bd7770ec1cd2a8d6c

--- js/graph.editor.js
+++ js/graph.editor.js
@@ -1,32 +1,24 @@
-( function( $ ) {
+(function( $ ) {
 	var oldContent = '';
 	(function() {
-		$('.mw-wiki-graph').each(function () {
-			// This should not be a loop - there must be only one value with this class
-			var el = this,
-				textbox = $('#wpTextbox1'),
-				context = textbox && textbox.data('wikiEditor-context');
+		try {
+			$('.mw-wiki-graph').each(function () {
+				// This should not be a loop - there must be only one value with this class
+				var el = this,
+					textbox = $('#wpTextbox1'),
+					context = textbox && textbox.data('wikiEditor-context');
 
-			if (!context || !context.fn || !context.fn.getContents) {
-				return;
-			}
-			var content = context.fn.getContents();
-			if (oldContent === content) {
-				return;
-			}
-			oldContent = content;
-			var spec = null;
-			try {
-				spec = $.parseJSON(content);
-			} catch(err) {
-			}
-			if (spec === null) {
-				return;
-			}
-			if (this.hasAttribute('spec'))
-				this.removeAttribute('spec');
-			vg.parse.spec(spec, function(chart) { chart({el:el}).update(); });
-		});
+				var content = context.fn.getContents();
+				if (oldContent === content) return;
+				oldContent = content;
+				var spec = $.parseJSON(content);
+				if (spec === null) return;
+				if (this.hasAttribute('spec')) this.removeAttribute('spec');
+				vg.parse.spec(spec, function(chart) { chart({el:el}).update(); });
+			});
+		} catch(err) {
+			// Ignore all errors
+		}
 		// FIXME: This should be done on data modification, not on timer
 		setTimeout(arguments.callee, 300);
 	})();
