68d2a2c3894022d2320e420d3f3d1735b7bca394
Yuri Astrakhan <yurik@wikimedia.org>
2015-01-26 12:03:36 -0800

    Fix graph preview editor
    Once again, thanks Krinkle for all your help!!!
    
    Change-Id: I5ae7728bfb1cb82e3454947f55f6a9d4b63157e4

--- js/graph.editor.js
+++ js/graph.editor.js
@@ -1,26 +1,25 @@
 (function( $ ) {
 	var oldContent = '';
-	(function() {
-		try {
-			$('.mw-wiki-graph').each(function () {
-				// This should not be a loop - there must be only one value with this class
-				var el = this,
-					textbox = $('#wpTextbox1'),
-					context = textbox && textbox.data('wikiEditor-context');
+	mw.hook('codeEditor.configure').add(function(session) {
+		function refreshGraph() {
+			try {
+				var el = $('.mw-wiki-graph').get(0),
+					content = session.getValue();
 
-				var content = context.fn.getContents();
 				if (oldContent === content) return;
 				oldContent = content;
+
 				var spec = $.parseJSON(content);
 				if (spec === null) return;
-				if (this.hasAttribute('spec')) this.removeAttribute('spec');
+
 				vg.parse.spec(spec, function (chart) {
 					chart({el: el}).update();
 				});
-			});
-		} finally {
-			// FIXME: This should be done on data modification, not on timer
-			setTimeout(arguments.callee, 300);
+			} finally {
+				// FIXME: This should be done on data modification, not on timer
+				setTimeout(refreshGraph, 300);
+			}
 		}
-	})();
+		refreshGraph();
+	});
 } ( jQuery ) );
