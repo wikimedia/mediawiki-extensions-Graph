a82f541df6d9812a9d5af99edd90b7bd1c97760a
Derk-Jan Hartman <hartman.wiki@gmail.com>
2015-05-09 18:51:58 +0200

    Start rendering graphs when the wiki content is ready
    * Updated by Yuri Astrakhan
    
    Bug: T98656
    Change-Id: I1799de6feb19ca7e109695d81d49c7911d1ba23c

--- js/graph.js
+++ js/graph.js
@@ -1,49 +1,52 @@
 ( function ( $, mw ) {
-	$( function () {
-		var originalSanitize,
-			specs = mw.config.get( 'wgGraphSpecs' );
+	var originalSanitize = false;
+	mw.hook( 'wikipage.content' ).add( function ( $content ) {
+		var specs = mw.config.get( 'wgGraphSpecs' );
 		if ( !specs ) {
 			return;
 		}
-		vg.config.domainWhiteList = mw.config.get( 'wgGraphDataDomains' );
-		vg.config.urlBlackList = mw.config.get( 'wgGraphUrlBlacklist' );
-		if ( !mw.config.get( 'wgGraphIsTrusted' ) ) {
-			vg.config.dataHeaders = { 'Treat-as-Untrusted': 1 };
-		}
-		vg.config.safeMode = vg.config.domainWhiteList !== false;
+		if ( originalSanitize === false ) {
+			// Make sure we only initialize graphs once
+			vg.config.domainWhiteList = mw.config.get( 'wgGraphDataDomains' );
+			vg.config.urlBlackList = mw.config.get( 'wgGraphUrlBlacklist' );
+			if ( !mw.config.get( 'wgGraphIsTrusted' ) ) {
+				vg.config.dataHeaders = { 'Treat-as-Untrusted': 1 };
+			}
+			vg.config.safeMode = vg.config.domainWhiteList !== false;
 
-		originalSanitize = vg.data.load.sanitizeUrl.bind( vg.data.load );
-		vg.data.load.sanitizeUrl = function ( urlOrig ) {
-			var url = originalSanitize.call( vg.data.load, urlOrig );
-			if ( !url ) {
-				return false;
-			}
-			// Normalize url by parsing and re-encoding it
-			url = new mw.Uri( url );
-			url.path = decodeURIComponent( url.path );
-			url = url.toString();
-			if ( !url ) {
-				return false;
-			}
-			if ( !vg.config.urlBlackListRe ) {
-				// Lazy initialize urlBlackListRe
-				if ( vg.config.urlBlackList ) {
-					vg.config.urlBlackListRe = vg.config.urlBlackList.map( function ( s ) {
-						return new RegExp( s );
-					} );
-				} else {
-					vg.config.urlBlackListRe = [];
+			originalSanitize = vg.data.load.sanitizeUrl.bind( vg.data.load );
+			vg.data.load.sanitizeUrl = function ( urlOrig ) {
+				var url = originalSanitize.call( vg.data.load, urlOrig );
+				if ( !url ) {
+					return false;
 				}
-			}
-			if ( vg.config.urlBlackListRe.some( function ( re ) {
-				return re.test( url );
-			} ) ) {
-				return false;
-			}
-			return url;
-		};
+				// Normalize url by parsing and re-encoding it
+				url = new mw.Uri( url );
+				url.path = decodeURIComponent( url.path );
+				url = url.toString();
+				if ( !url ) {
+					return false;
+				}
+				if ( !vg.config.urlBlackListRe ) {
+					// Lazy initialize urlBlackListRe
+					if ( vg.config.urlBlackList ) {
+						vg.config.urlBlackListRe = vg.config.urlBlackList.map( function ( s ) {
+							return new RegExp( s );
+						} );
+					} else {
+						vg.config.urlBlackListRe = [];
+					}
+				}
+				if ( vg.config.urlBlackListRe.some( function ( re ) {
+					return re.test( url );
+				} ) ) {
+					return false;
+				}
+				return url;
+			};
+		}
 
-		$( '.mw-wiki-graph' ).each( function () {
+		$content.find( '.mw-wiki-graph' ).each( function () {
 			var graphId = $( this ).data( 'graph-id' ),
 				el = this;
 			if ( !specs[graphId] ) {
