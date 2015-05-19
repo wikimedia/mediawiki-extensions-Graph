09df61c8f19601c80c1bd3ff8ea090c6208742c8
Derk-Jan Hartman <hartman.wiki@gmail.com>
2015-05-19 21:18:31 +0200

    CI: add npm test, for Jenkins
    Change-Id: I7ebfcc1da89147e24d5bee8b72628ea249a882cb

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,5 +32,6 @@ module.exports = function ( grunt ) {
 	} );
 
 	grunt.registerTask( 'lint', [ 'jshint', 'jscs' ] );
-	grunt.registerTask( 'default', 'lint' );
+	grunt.registerTask( 'test', 'lint' );
+	grunt.registerTask( 'default', 'test' );
 };
