1aeb0845e350c6843dd198884cd8d1d9eccd929c
Julien Girault <jgirault@wikimedia.org>
2016-04-01 16:20:30 +0300

    Fix the scrollbar issue on the GraphSandbox + a couple more UI improvements (adjust the sandbox to the viewport, and some styling)
    Bug: T127010
    Change-Id: I3b273473c8c280df5b8770a92c99c54235a58131

--- modules/graph.sandbox.js
+++ modules/graph.sandbox.js
@@ -1,16 +1,36 @@
 ( function ( $, mw ) {
-	var oldContent, ccw;
+	var oldContent, ccw, $rightSplitTop,
+		resizeCodeEditor = $.noop;
 
 	$( function () {
-		$( '#mw-graph-sandbox' ).width( '100%' ).height( 700 ).split( { orientation: 'vertical', limit: 100, position: '40%' } );
-		$( '#mw-graph-left' ).split( { orientation: 'horizontal', limit: 100, position: '90%' } );
-		$( '#mw-graph-right' ).split( { orientation: 'horizontal', limit: 100, position: '90%' } );
+		var viewportHeight = $( window ).height(),
+			sandboxHeight = viewportHeight - 150,
+			initialPosition = sandboxHeight - 100;
+		$( '#mw-graph-sandbox' ).width( '100%' ).height( sandboxHeight ).split( {
+			orientation: 'vertical',
+			limit: 100,
+			position: '40%'
+		} );
+		$( '#mw-graph-left' ).split( {
+			orientation: 'horizontal',
+			limit: 100,
+			position: initialPosition
+		} );
+		$( '#mw-graph-right' ).split( {
+			orientation: 'horizontal',
+			limit: 100,
+			position: initialPosition,
+			onDrag: function () {
+				resizeCodeEditor( $rightSplitTop.height() );
+			}
+		} );
 	} );
 
 	mw.hook( 'codeEditor.configure' ).add( function ( session ) {
 		var $errorLog = $( '#mw-graph-log' )[ 0 ],
 			$json = $( '#mw-graph-json' )[ 0 ],
-			$graph = $( '.mw-graph' )[ 0 ];
+			$graph = $( '.mw-graph' )[ 0 ],
+			$editor = $( '.editor' );
 
 		if ( ccw ) {
 			ccw.release();
@@ -21,6 +41,14 @@
 			},
 			message: mw.msg( 'editwarning-warning' )
 		} );
+
+		resizeCodeEditor = function ( height ) {
+			$editor.parent().height( height - 57 );
+			$.wikiEditor.instances[ 0 ].data( 'wikiEditor-context' ).codeEditor.resize();
+		};
+
+		$rightSplitTop = $( '#mw-graph-right .top_panel' );
+		resizeCodeEditor( $rightSplitTop.height() );
 
 		session.on( 'change', $.debounce( 300, function () {
 			var content = session.getValue();
