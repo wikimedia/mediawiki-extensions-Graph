e4a1db3344d168ede33a4904e98e06841e4a472d
petarpetkovic <ppetkovic@wikimedia.org>
2019-05-13 03:28:43 +0200

    Add Serbian aliases
    Bug: T212586
    Change-Id: Iec1b71f53d3cd773fcc99b5062826cebf42d2169

--- Graph.alias.php
+++ Graph.alias.php
@@ -17,6 +17,16 @@ $specialPageAliases['cs'] = [
 	'GraphSandbox' => [ 'Pískoviště_grafů' ],
 ];
 
+/** Serbian Cyrillic (српски (ћирилица)) */
+$specialPageAliases['sr-ec'] = [
+	'GraphSandbox' => [ 'ПесакЗаГрафиконе' ],
+];
+
+/** Serbian Latin (srpski (latinica)) */
+$specialPageAliases['sr-el'] = [
+	'GraphSandbox' => [ 'PesakZaGrafikone' ],
+];
+
 /** Urdu (اردو) */
 $specialPageAliases['ur'] = [
 	'GraphSandbox' => [ 'گراف_کا_تختہ_مشق' ],
