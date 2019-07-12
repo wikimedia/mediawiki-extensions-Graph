9d5af2bafaa6081a282a5dab8f9307bef441e423
James D. Forrester <jforrester@wikimedia.org>
2019-07-12 11:43:52 -0700

    build: Modernise linters
    Change-Id: I2887b1362ba26ec3d5c42ed1c5fcd52ac7874d35

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,24 +9,21 @@
 module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'extension.json' );
 
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		eslint: {
 			options: {
 				reportUnusedDisableDirectives: true,
+				extensions: [ '.js', '.json' ],
 				cache: true
 			},
 			all: [
-				'*.js',
-				'**/*.js',
+				'**/*.js{,on}',
 				'!lib/**',
-				'!node_modules/**',
-				'!vendor/**'
+				'!{node_modules,vendor}/**'
 			]
 		},
 		stylelint: {
@@ -34,32 +31,15 @@ module.exports = function ( grunt ) {
 				syntax: 'less'
 			},
 			all: [
-				'**/*.css',
-				'**/*.less',
+				'**/*.{c,le}ss',
 				'!lib/**',
-				'!node_modules/**',
-				'!vendor/**'
+				'!{node_modules,vendor}/**'
 			]
 		},
-		watch: {
-			files: [
-				'.{stylelintrc,eslintrc}.json',
-				'<%= eslint.all %>',
-				'<%= stylelint.all %>'
-			],
-			tasks: 'lint'
-		},
-		banana: conf.MessagesDirs,
-		jsonlint: {
-			all: [
-				'**/*.json',
-				'!node_modules/**',
-				'!vendor/**'
-			]
-		}
+		banana: conf.MessagesDirs
 	} );
 
-	grunt.registerTask( 'lint', [ 'eslint', 'jsonlint', 'stylelint', 'banana' ] );
+	grunt.registerTask( 'lint', [ 'eslint', 'banana', 'stylelint' ] );
 	grunt.registerTask( 'test', 'lint' );
 	grunt.registerTask( 'default', 'test' );
 };
