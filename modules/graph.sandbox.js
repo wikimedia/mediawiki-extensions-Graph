a7aa25400c267aa525b8a0f2ff3074d9860511d0
Derk-Jan Hartman <hartman.wiki@gmail.com>
2022-01-22 21:11:32 +0100

    Replace jquery.debounce with OO.ui.debounce
    Bug: T213426
    Change-Id: Iccb2a8d375fc96bf8bcac1c555f966fa2e7560b8

--- modules/graph.sandbox.js
+++ modules/graph.sandbox.js
@@ -42,7 +42,7 @@
 		// I tried to resize on $( window ).resize(), but that didn't work right
 		resizeCodeEditor();
 
-		session.on( 'change', $.debounce( 300, function () {
+		session.on( 'change', OO.ui.debounce( function () {
 			var content = session.getValue();
 
 			if ( oldContent === content ) {
@@ -69,7 +69,7 @@
 			} ).fail( function ( errCode, error ) {
 				$graphEl.textContent = errCode.toString() + ':' + ( error.exception || error ).toString();
 			} );
-		} ) );
+		}, 300 ) );
 	} );
 
 }() );
