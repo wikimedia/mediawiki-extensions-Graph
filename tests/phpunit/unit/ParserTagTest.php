c3be47f30770456742f4271746c70849cf02b212
Jon Robson <jdlrobson@gmail.com>
2023-04-26 16:12:39 -0700

    Add tests for outdated graph categorization
    Bug: T335127
    Bug: T315816
    Change-Id: Id8dd6287dee085455a684a0b75e3fb668f363b26

---
+++ tests/phpunit/unit/ParserTagTest.php
@@ -0,0 +1,81 @@
+<?php
+namespace Graph\Tests\Unit;
+
+use Graph\ParserTag;
+use Parser;
+use ParserOptions;
+use ParserOutput;
+
+/**
+ * Class ParserTagTestt
+ * @package Graph\Tests\Unit
+ * @coversDefaultClass \Graph\ParserTag
+ * @group Graph
+ * @group Extensions
+ */
+class ParserTagTest extends \MediaWikiUnitTestCase {
+	private function mockParserOutput() {
+		$mockParserOutput = $this->getMockBuilder( ParserOutput::class )
+			->disableOriginalConstructor()
+			->onlyMethods( [ 'setExtensionData', 'getExtensionData' ] )
+			->getMock();
+
+		$parameters = [];
+		$mockParserOutput->expects( $this->any() )->method( 'setExtensionData' )
+			->willReturnCallback( static function ( $key, $value ) use ( &$parameters ) {
+				$parameters[$key] = $value;
+			} );
+		$mockParserOutput->expects( $this->any() )->method( 'getExtensionData' )
+			->willReturnCallback( static function ( $key ) use ( &$parameters ) {
+				return $parameters[$key] ?? null;
+			} );
+		return $mockParserOutput;
+	}
+
+	private function mockParserOptions() {
+		return $this->getMockBuilder( ParserOptions::class )
+			->disableOriginalConstructor()
+			->getMock();
+	}
+
+	private function mockParser() {
+		return $this->getMockBuilder( Parser::class )
+			->disableOriginalConstructor()
+			->getMock();
+	}
+
+	/**
+	 * @covers ::buildHtml
+	 */
+	public function testBuildHtmlLogsObsoleteGraphs() {
+		$mockParser = $this->mockParser();
+		$mockParserOptions = $this->mockParserOptions();
+		$mockParserOutput = $this->mockParserOutput();
+		$parser = new ParserTag( $mockParser, $mockParserOptions, $mockParserOutput );
+		$json = '{ "version": 2 }';
+		$str = $parser->buildHtml( $json, 1 );
+		$this->assertTrue( $mockParserOutput->getExtensionData( 'graph_vega2' ) );
+		$this->assertTrue( $mockParserOutput->getExtensionData( 'graph_specs_obsolete' ) );
+	}
+
+	/**
+	 * @covers ::buildHtml
+	 */
+	public function testBuildHtmlValidGraph() {
+		$mockParser = $this->mockParser();
+		$mockParserOptions = $this->mockParserOptions();
+		$mockParserOutput = $this->mockParserOutput();
+
+		$parser = new ParserTag( $mockParser, $mockParserOptions, $mockParserOutput );
+		$json = '{ "version": 5 }';
+		$str = $parser->buildHtml( $json, 1 );
+		$this->assertTrue( $mockParserOutput->getExtensionData( 'graph_vega5' ) );
+
+		// create new parser with cleared parser output
+		$mockParserOutput = $this->mockParserOutput();
+		$parser = new ParserTag( $mockParser, $mockParserOptions, $mockParserOutput );
+		$json = '{ "$schema": "https://vega.github.io/schema/vega/v5.json" }';
+		$str = $parser->buildHtml( $json, 1 );
+		$this->assertTrue( $mockParserOutput->getExtensionData( 'graph_vega5' ) );
+	}
+}
